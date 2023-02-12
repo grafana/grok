c4cb22e60cd6c3fd055df165d33b2098bc8d718f
Malcolm Holmes <mdh@odoko.co.uk>
2023-02-12 08:44:00 +0100

    Generate jsonschema within Jsonnet jennies

---
+++ gen/jsonnet/imports.go
@@ -0,0 +1,121 @@
+package jsonnet
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/grafana/codejen"
+	"github.com/grafana/grafana/pkg/kindsys"
+)
+
+// Hack to make the Generate function re-entrant
+var tempGlobalImportsBuf strings.Builder
+
+/*
+type JsonnetImportsCoreJenny struct{}
+
+func (j JsonnetImportsCoreJenny) JennyName() string {
+        return "JsonnetImportsCoreJenny"
+}
+
+var tempGlobalImports strings.Builder
+
+func (j *JsonnetImportsCoreJenny) Generate(k ...kindsys.Kind) (*codejen.File, error) {
+        grafanaVersion := os.Getenv("GRAFANA_VERSION")
+*/
+type JsonnetCoreImportsJenny struct{}
+
+func (j JsonnetCoreImportsJenny) JennyName() string {
+	return "JsonnetCoreImportsJenny"
+}
+
+func (j *JsonnetCoreImportsJenny) Generate(k ...kindsys.Kind) (*codejen.File, error) {
+	grafanaVersion := os.Getenv("GRAFANA_VERSION")
+	if grafanaVersion == "" {
+		return nil, nil
+	}
+	tempGlobalImportsBuf.WriteString("[\n")
+	for _, kind := range k {
+		generateImports(kind)
+	}
+	return nil, nil
+}
+
+type JsonnetComposableImportsJenny struct{}
+
+func (j JsonnetComposableImportsJenny) JennyName() string {
+	return "JsonnetImportsCoreJenny"
+}
+
+func (j *JsonnetComposableImportsJenny) Generate(k ...kindsys.Composable) (*codejen.File, error) {
+	grafanaVersion := os.Getenv("GRAFANA_VERSION")
+	if grafanaVersion == "" {
+		return nil, nil
+	}
+	for _, kind := range k {
+		generateImports(kind)
+	}
+	tempGlobalImportsBuf.WriteString("]\n")
+	filename := fmt.Sprintf("%s/imports.libsonnet", grafanaVersion)
+	return codejen.NewFile(filename, []byte(tempGlobalImportsBuf.String()), j), nil
+
+	//return generateImports(k)
+}
+
+func generateImports(kind kindsys.Kind) {
+	var schemaVersion string
+	if kind.Maturity().Less(kindsys.MaturityStable) {
+		schemaVersion = "x"
+	} else {
+		schemaVersion = kind.Lineage().Latest().Version().String()
+	}
+	// @TODO we should be receiving a name without schema interface type so that we don't
+	// need to strip it with a hack like this:
+	name := fixKindName(kind.MachineName())
+	comp, isComposable := kind.(kindsys.Composable)
+	var importStatement string
+	if isComposable {
+		schemaInterface := strings.ToLower(comp.Def().Properties.SchemaInterface)
+		importStatement = fmt.Sprintf("  import \"./kinds/composable/%s/%s/%s/%s_types_gen.json\",\n",
+			name, schemaInterface, schemaVersion, name)
+	} else {
+		importStatement = fmt.Sprintf("  import \"./kinds/core/%s/%s/%s_types_gen.json\",\n",
+			name, schemaVersion, name)
+	}
+	tempGlobalImportsBuf.WriteString(importStatement)
+
+}
+
+/*
+type JsonnetImportsComposableJenny struct{}
+
+func (j JsonnetImportsComposableJenny) JennyName() string {
+	return "JsonnetImportsComposableJenny"
+}
+
+func (j *JsonnetImportsComposableJenny) Generate(k ...kindsys.Composable) (*codejen.File, error) {
+	grafanaVersion := os.Getenv("GRAFANA_VERSION")
+	if grafanaVersion == "" {
+		return nil, nil
+	}
+	buf := strings.Builder{}
+	buf.WriteString("[\n")
+	buf.WriteString(tempGlobalImports.String())
+	for _, kind := range k {
+		var schemaVersion string
+		if kind.Maturity().Less(kindsys.MaturityStable) {
+			schemaVersion = "x"
+		} else {
+			schemaVersion = kind.Lineage().Latest().Version().String()
+		}
+		name := kindName(kind)
+		schemaInterface := schemaInterfaceName(kind)
+		importStatement := fmt.Sprintf("  import \"./kinds/composable/%s/%s/%s/%s_types_gen.json\",\n",
+			name, schemaInterface, schemaVersion, name)
+		buf.WriteString(importStatement)
+
+	}
+
+}
+*/
--- gen/jsonnet/jsonnet.go
+++ gen/jsonnet/jsonnet.go
@@ -1,168 +1,40 @@
 package jsonnet
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
-	"github.com/grafana/codejen"
-	"github.com/grafana/grafana/pkg/kindsys"
+	"github.com/grafana/grafana/pkg/codegen"
 	"github.com/grafana/grok/internal/jen"
 )
 
 func JenniesForJsonnet() jen.TargetJennies {
 	tgt := jen.NewTargetJennies()
 
+	grafanaVersion := os.Getenv("GRAFANA_VERSION")
+	if grafanaVersion == "" {
+		return tgt
+	}
+	log.Print("CORE")
 	tgt.Core.Append(
-		&JsonnetImportsCoreJenny{},
-		&JsonnetFileCoreJenny{},
+		&JsonnetCoreImportsJenny{},
+		codegen.LatestMajorsOrXJenny(filepath.Join(grafanaVersion, "kinds", "core"), JsonnetSchemaJenny{}),
 	)
-
+	log.Print("COMP")
 	tgt.Composable.Append(
-		&JsonnetImportsComposableJenny{},
-		&JsonnetFileComposableJenny{},
+		// oooonly need to inject the proper path interstitial to make this right
+		&JsonnetComposableImportsJenny{},
+		jen.ComposableLatestMajorsOrXJenny(filepath.Join(grafanaVersion, "kinds", "composable"), JsonnetSchemaJenny{}),
 	)
 
 	return tgt
 }
 
-type JsonnetImportsCoreJenny struct{}
-
-func (j JsonnetImportsCoreJenny) JennyName() string {
-	return "JsonnetImportsCoreJenny"
-}
-
-var tempGlobalImports strings.Builder
-
-func (j *JsonnetImportsCoreJenny) Generate(k ...kindsys.Kind) (*codejen.File, error) {
-	grafanaVersion := os.Getenv("GRAFANA_VERSION")
-	if grafanaVersion == "" {
-		return nil, nil
-	}
-	buf := strings.Builder{}
-	buf.WriteString("[\n")
-	for _, kind := range k {
-		var schemaVersion string
-		if kind.Maturity().Less(kindsys.MaturityStable) {
-			schemaVersion = "x"
-		} else {
-			schemaVersion = kind.Lineage().Latest().Version().String()
-		}
-		importStatement := fmt.Sprintf("  import \"github.com/grafana/grok/jsonschema/kinds/core/%s/%s/%s_types_gen.json\",\n",
-			kind.Lineage().Name(), schemaVersion, kind.Lineage().Name())
-		buf.WriteString(importStatement)
-		tempGlobalImports.WriteString(importStatement)
-	}
-	buf.WriteString("]\n")
-	return nil, nil
-}
-
-type JsonnetImportsComposableJenny struct{}
-
-func (j JsonnetImportsComposableJenny) JennyName() string {
-	return "JsonnetImportsComposableJenny"
-}
-
-func (j *JsonnetImportsComposableJenny) Generate(k ...kindsys.Composable) (*codejen.File, error) {
-	grafanaVersion := os.Getenv("GRAFANA_VERSION")
-	if grafanaVersion == "" {
-		return nil, nil
-	}
-	buf := strings.Builder{}
-	buf.WriteString("[\n")
-	buf.WriteString(tempGlobalImports.String())
-	for _, kind := range k {
-		var schemaVersion string
-		if kind.Maturity().Less(kindsys.MaturityStable) {
-			schemaVersion = "x"
-		} else {
-			schemaVersion = kind.Lineage().Latest().Version().String()
-		}
-		importStatement := fmt.Sprintf("  import \"github.com/grafana/grok/jsonschema/kinds/composable/%s/%s/%s_types_gen.json\",\n",
-			kind.Lineage().Name()+"/"+strings.ToLower(kind.Def().Properties.SchemaInterface), schemaVersion, kind.Lineage().Name())
-		buf.WriteString(importStatement)
-	}
-	buf.WriteString("]\n")
-	filename := fmt.Sprintf("%s/imports.libsonnet", grafanaVersion)
-	return codejen.NewFile(filename, []byte(buf.String()), j), nil
-}
-
-const (
-	jsonnetfilePre = `
-{
-	"version": 1,
-	"dependencies": [
-`
-	jsonnetfilePost = `
-	],
-	"legacyImports": false
-  }
-`
-	jsonnetfileEntry = `
-	{
-		"source": {
-		  "git": {
-			"remote": "https://github.com/grafana/grok.git",
-			"subdir": "jsonschema/kinds/%s/%s/%s"
-		  }
-		},
-		"version": "main"
-	}`
-)
-
-type JsonnetFileCoreJenny struct{}
-
-func (j JsonnetFileCoreJenny) JennyName() string {
-	return "JsonnetFileCoreJenny"
-}
-
-var tempGlobalJsonnetFileEntries []string
-
-func (j *JsonnetFileCoreJenny) Generate(k ...kindsys.Kind) (*codejen.File, error) {
-	grafanaVersion := os.Getenv("GRAFANA_VERSION")
-	if grafanaVersion == "" {
-		return nil, nil
-	}
-	for _, kind := range k {
-		var schemaVersion string
-		if kind.Maturity().Less(kindsys.MaturityStable) {
-			schemaVersion = "x"
-		} else {
-			schemaVersion = kind.Lineage().Latest().Version().String()
-		}
-		entry := fmt.Sprintf(jsonnetfileEntry, "core", kind.Lineage().Name(), schemaVersion)
-		tempGlobalJsonnetFileEntries = append(tempGlobalJsonnetFileEntries, entry)
-	}
-	return nil, nil
-}
-
-type JsonnetFileComposableJenny struct{}
-
-func (j JsonnetFileComposableJenny) JennyName() string {
-	return "JsonnetFileComposableJenny"
-}
-
-func (j *JsonnetFileComposableJenny) Generate(k ...kindsys.Composable) (*codejen.File, error) {
-	grafanaVersion := os.Getenv("GRAFANA_VERSION")
-	if grafanaVersion == "" {
-		return nil, nil
-	}
-	buf := strings.Builder{}
-	buf.WriteString(jsonnetfilePre)
-	entries := []string{}
-	entries = append(entries, tempGlobalJsonnetFileEntries...)
-	for _, kind := range k {
-		var schemaVersion string
-		if kind.Maturity().Less(kindsys.MaturityStable) {
-			schemaVersion = "x"
-		} else {
-			schemaVersion = kind.Lineage().Latest().Version().String()
-		}
-		entry := fmt.Sprintf(jsonnetfileEntry, "composable", kind.Lineage().Name()+"/"+strings.ToLower(kind.Def().Properties.SchemaInterface), schemaVersion)
-		entries = append(entries, entry)
-	}
-	buf.WriteString(strings.Join(entries, ",\n"))
-	buf.WriteString(jsonnetfilePost)
-	filename := fmt.Sprintf("%s/jsonnetfile.json", grafanaVersion)
-	return codejen.NewFile(filename, []byte(buf.String()), j), nil
+func fixKindName(name string) string {
+	name = strings.ToLower(name)
+	name = strings.ReplaceAll(name, "panelcfg", "")
+	name = strings.ReplaceAll(name, "dataquery", "")
+	return name
 }
