4314d962b595512322a969f863bbde6a23c874b7
Kévin Gomez <kevin.gomez@grafana.com>
2023-06-29 10:47:56 +0200

    Add a flag to disable the generation of specified kinds

--- cmd/generate/command.go
+++ cmd/generate/command.go
@@ -3,6 +3,7 @@ package generate
 import (
 	"context"
 	"fmt"
+	"io/fs"
 	"path/filepath"
 
 	"cuelang.org/go/cue/cuecontext"
@@ -15,11 +16,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type kindGenerator func(opts options, themaRuntime *thema.Runtime, commonFS fs.FS, targetJennies jen.TargetJennies) (*codejen.FS, error)
+
 type options struct {
 	kindRegistryRoot string
 	targetVersion    string
 
 	outputDir string
+
+	// Do not run the generation process for these kinds
+	excludeKinds []string
 }
 
 func (opts options) grafanaRegistryRoot() string {
@@ -42,9 +48,8 @@ func Command() *cobra.Command {
 	opts := options{}
 
 	cmd := &cobra.Command{
-		Use:   "generate REGISTRY_PATH SCHEMA_VERSION",
-		Short: "TBD",
-		Args:  cobra.ExactArgs(2),
+		Use:  "generate REGISTRY_PATH SCHEMA_VERSION",
+		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts.kindRegistryRoot = args[0]
 			opts.targetVersion = args[1]
@@ -54,12 +59,17 @@ func Command() *cobra.Command {
 	}
 
 	cmd.Flags().StringVarP(&opts.outputDir, "output", "o", ".", "Output directory")
+	cmd.Flags().StringSliceVar(&opts.excludeKinds, "exclude-kind", nil, "Excludes a kind from the code generation process.")
 
 	return cmd
 }
 
 func doGenerate(opts options) error {
 	themaRuntime := thema.NewRuntime(cuecontext.New())
+	kindGenerators := map[string]kindGenerator{
+		"core":       generateCoreKinds,
+		"composable": generateComposableKinds,
+	}
 
 	fmt.Printf("Loading grafana-schema/common module from '%s'\n", opts.commonLibPath())
 	commonCueImportPrefix := "cue.mod/pkg/github.com/grafana/grafana/packages/grafana-schema/src/common"
@@ -68,48 +78,66 @@ func doGenerate(opts options) error {
 		return err
 	}
 
-	fmt.Printf("Building core Kinds from CUE files in '%s'\n", opts.corePath())
-	coreKinds, err := loadCoreKinds(themaRuntime, opts.corePath())
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Building composable Kinds from CUE files in '%s'\n", opts.composablePath())
-	composableKinds, err := loadComposableKinds(themaRuntime, commonFS, opts.composablePath())
-	if err != nil {
-		return err
-	}
-
 	// Here begins the code generation setup
 	rootCodeJenFS := codejen.NewFS()
 	targetJennies := lineUpJennies("v" + opts.targetVersion)
 
-	fmt.Printf("Got %d core kinds\n", len(coreKinds))
-	fmt.Printf("Got %d composable kinds\n", len(composableKinds))
+	// Run the generation process for the desired kinds
+	for kind, generator := range kindGenerators {
+		if contains(opts.excludeKinds, kind) {
+			continue
+		}
 
-	coreKindFS, err := targetJennies.Core.GenerateFS(coreKinds...)
-	if err != nil {
-		return fmt.Errorf("could not generate FS for core kind: %w", err)
+		fmt.Printf("Building '%s' kinds from CUE files in '%s'\n", kind, opts.corePath())
+
+		kindFS, err := generator(opts, themaRuntime, commonFS, targetJennies)
+		if err != nil {
+			return err
+		}
+
+		if err = rootCodeJenFS.Merge(kindFS); err != nil {
+			return fmt.Errorf("could not merge kind FS into root FS: %w", err)
+		}
 	}
 
-	if err = rootCodeJenFS.Merge(coreKindFS); err != nil {
-		return fmt.Errorf("could not merge coreKindFS into rootCodeJenFS: %w", err)
-	}
-
-	composableKindFS, err := targetJennies.Composable.GenerateFS(composableKinds...)
-	if err != nil {
-		panic(fmt.Errorf("could not generate FS for composable kind: %w", err))
-	}
-
-	if err = rootCodeJenFS.Merge(composableKindFS); err != nil {
-		return fmt.Errorf("could not merge composableKindFS into rootCodeJenFS: %w", err)
-	}
-
+	// Write the output to disk
 	if err = rootCodeJenFS.Write(context.Background(), opts.outputDir); err != nil {
 		return fmt.Errorf("could not write rootCodeJenFS to disk: %w", err)
 	}
 
 	return nil
+}
+
+func generateCoreKinds(opts options, themaRuntime *thema.Runtime, _ fs.FS, targetJennies jen.TargetJennies) (*codejen.FS, error) {
+	coreKinds, err := loadCoreKinds(themaRuntime, opts.corePath())
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Got %d core kinds\n", len(coreKinds))
+
+	coreKindFS, err := targetJennies.Core.GenerateFS(coreKinds...)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate FS for core kind: %w", err)
+	}
+
+	return coreKindFS, nil
+}
+
+func generateComposableKinds(opts options, themaRuntime *thema.Runtime, commonFS fs.FS, targetJennies jen.TargetJennies) (*codejen.FS, error) {
+	composableKinds, err := loadComposableKinds(themaRuntime, commonFS, opts.composablePath())
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Got %d composable kinds\n", len(composableKinds))
+
+	composableKindFS, err := targetJennies.Composable.GenerateFS(composableKinds...)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate FS for composable kind: %w", err)
+	}
+
+	return composableKindFS, nil
 }
 
 // Line up all the jennies from all the language targets, prefixing them with
@@ -132,4 +160,14 @@ func lineUpJennies(targetGrafanaVersion string) jen.TargetJennies {
 	}
 
 	return targets
+}
+
+func contains(haystack []string, needle string) bool {
+	for _, v := range haystack {
+		if v == needle {
+			return true
+		}
+	}
+
+	return false
 }
